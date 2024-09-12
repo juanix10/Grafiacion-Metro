e6c758012bc2450a8c086980a166e0764a447134
ELRAE011 <erayala15@gmail.com>
2024-09-12 17:37:20 -0600

    Actualizacion logos

--- proyecto/ConexionCSV.py
+++ proyecto/ConexionCSV.py
@@ -5,7 +5,7 @@ import mysql.connector
 conn = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="123",
+    password="admin123",
     database="metro"
 )
 cursor = conn.cursor()
